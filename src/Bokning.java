33bf5ec56663e2edb53bb9ea78b0dbe7dea31211
Kiara <ahmadnejadkiara@gmail.com>
2023-04-24 10:28:58 +0200

    börjat med kod

---
+++ src/Bokning.java
@@ -0,0 +1,27 @@
+
+
+public class Bokning {
+
+    public Bokning(){
+        System.out.println("Välkommen till Stockholms Hunddagis");
+        
+        kundNamn();
+        hundNamn();
+        pris();
+        tid();
+    }
+
+    private void tid() {
+    }
+
+    private void pris() {
+    }
+
+    private void hundNamn() {
+    }
+
+    private void kundNamn() {
+    }
+    
+    
+}
